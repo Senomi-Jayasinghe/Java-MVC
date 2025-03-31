5c00398112d455bc37a9f374e3b9996744b4e5ac
[Senomi-Jayasinghe] <jayasinghesr@students.nsbm.ac.lk>
2025-03-31 21:23:23 +0530

    Create DbConnection.java

---
+++ MVCExample/src/java/bookPackage/DbConnection.java
@@ -0,0 +1,17 @@
+package bookPackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+     //PUT ALL DATABASE PARAMETERS TO ONE PLACE
+    private static final String URL = "jdbc:mysql://localhost:3306/book";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); //LOAD JDBC DRIVER
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
