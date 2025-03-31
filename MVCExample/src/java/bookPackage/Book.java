afd5eb9bb7f216ae877db38d36f2a29f13c1df80
[Senomi-Jayasinghe] <jayasinghesr@students.nsbm.ac.lk>
2025-03-31 21:42:16 +0530

    Corrected Book.java

--- MVCExample/src/java/bookPackage/Book.java
+++ MVCExample/src/java/bookPackage/Book.java
@@ -1,6 +1,18 @@
 package bookPackage;
 public class Book {
     
+    private int BookID;
+    private String BookName;
+    
+    public Book() {}
+    
+    // Parameterized Constructor
+    public Book(int BookID, String BookName) {
+        this.BookID = BookID;
+        this.BookName = BookName;
+    }
+    
+    //GETTERS AND SETTERS FOR ATTRIBUTES
     public int getBookID() {
         return BookID;
     }
@@ -16,7 +28,4 @@ public class Book {
     public void setBookName(String BookName) {
         this.BookName = BookName;
     }
-    
-    private int BookID;
-    private String BookName;
 }
